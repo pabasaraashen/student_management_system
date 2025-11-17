cd2ef417795f29a9e5e2cb763ba23ec745b9d5bb
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-11-17 13:08:46 +0530

    Update Student.java

--- src/main/java/Data/Student.java
+++ src/main/java/Data/Student.java
@@ -9,5 +9,38 @@ package Data;
  * @author pabas
  */
 public class Student {
-    
+    private String FirstName;
+    private String LastName;
+    private String Gender;
+    private int age;
+    private int grade;
+
+    public Student(String FirstName, String LastName, String Gender, int age, int grade) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Gender = Gender;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+      
 }
